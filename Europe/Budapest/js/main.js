be6158ec5266f83d7f23a537274185528bfe1d13
MiyukiAbe <miyuki0809@gmail.com>
2018-01-28 23:54:11 -0500

    googleMap markers and images are linked now
    still need to work on:
    1: Style
    2: custamized marker
    3: would like to list up all pictures on the bottom and would like to make it clickable.

--- Europe/Budapest/js/main.js
+++ Europe/Budapest/js/main.js
@@ -2,9 +2,43 @@ $(document).ready(function(){
 
 
 
-$("#map").on("click", "span, .location1", function() {
-alert("hello world!")
+$("#map").on("click", "span", function() {
+	$("#placeHolder").css("display","none");
+	$("#showImage").show();
+	$(".show").css("display", "none");
+		if($(this).hasClass("location1") ){
+			$(".location1").show();
+	}	if($(this).hasClass("location2") ){
+			$(".location2").show();
+	}	if($(this).hasClass("location3") ){
+			$(".location3").show();
+	}	if($(this).hasClass("location4") ){
+			$(".location4").show();
+	}	if($(this).hasClass("location5") ){
+			$(".location5").show();
+	}	if($(this).hasClass("location6") ){
+			$(".location6").show();
+	}	
 });
+
+
+
+
+//When user click marker, 
+//Hide .showImage
+//if the marker has class "locaiton1"
+//show .location1
+//if the marker has class "location2"
+//show .location2
+
+
+
+
+
+
+
+
+
 
 
   });
